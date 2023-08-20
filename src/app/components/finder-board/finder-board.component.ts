9ff52acec20267f4cdc320b2783cd3b3919d9a33
Antonio Cordoba <acordoba@ntst.com>
2023-08-20 11:33:53 -0400

    - re-styled

--- src/app/components/finder-board/finder-board.component.ts
+++ src/app/components/finder-board/finder-board.component.ts
@@ -10,10 +10,11 @@ export class FinderBoardComponent {
 
   constructor (private positionService: PositionService) {}
 
+  gpsSpot = '';
   currentAddressString = '';
   first: any;
   found: any;
-
+  
   searchAddressString(searchString: string) {
     console.log(searchString);
     this.positionService.AddressSearch(searchString)
@@ -22,20 +23,21 @@ export class FinderBoardComponent {
         console.log(info);
         if ((info) && (info['results']) && (info['results']['length'] > 0)){
           this.first = (info as any).results[0];
-          const gpsSpot = `${this.first?.geometry.location.lat}, ${this.first?.geometry.location.lng}`; 
+          this.gpsSpot = `${this.first?.geometry.location.lat}, ${this.first?.geometry.location.lng}`; 
           this.found = { 
             located_address: this.first.formatted_address, 
-            coordinates: gpsSpot
+            coordinates: this.gpsSpot
           };
           console.log(this.first);
-          console.log(gpsSpot);
-          navigator.clipboard.writeText(gpsSpot)
-          .then(() => null);
+          console.log(this.gpsSpot);
         }
       }, 
       error: er => this.first = er,
       complete: () => null
     });
   }
-
+  copyAddress() {
+    navigator.clipboard.writeText(this.gpsSpot)
+    .then(() => null);
+  }
 }
